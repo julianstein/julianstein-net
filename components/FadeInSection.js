e5f6fa0954b5e9c9fc9387ee536e11848616b285
julianstein <julian.stein@gmail.com>
2021-01-19 14:28:14 -0800

    fade in section

--- components/FadeInSection.js
+++ components/FadeInSection.js
@@ -3,11 +3,45 @@ import { useEffect, useRef, useState } from 'react';
 const FadeInSection = (props) => {
   const [isVisible, setVisible] = useState(true);
   const domRef = useRef();
+
+  let options = {
+    rootMargin: '0px',
+    threshold: 0
+  };
+
   useEffect(() => {
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => setVisible(entry.isIntersecting));
-    });
-    observer.observe(domRef.current);
+    let previousY = 0;
+    let previousRatio = 0;
+
+    const observer = new IntersectionObserver(([entry]) => {
+      //console.log(entry);
+
+      const currentY = entry.boundingClientRect.y;
+      const currentRatio = entry.intersectionRatio;
+      const isIntersecting = entry.isIntersecting;
+
+      // Scrolling down/up
+      if (currentY > previousY) {
+        if (currentRatio < previousRatio && isIntersecting) {
+          setVisible(entry.isIntersecting);
+        } else {
+          setVisible(entry.isIntersecting);
+        }
+      } else if (currentY < previousY && isIntersecting) {
+        if (currentRatio > previousRatio) {
+          setVisible(entry.isIntersecting);
+        } else {
+          setVisible(entry.isIntersecting);
+        }
+      }
+
+      previousY = currentY;
+      previousRatio = currentRatio;
+    }, options);
+
+    if (domRef) {
+      observer.observe(domRef.current);
+    }
     return () => observer.unobserve(domRef.current);
   }, []);
   return (
