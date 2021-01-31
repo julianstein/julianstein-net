db057951e5324b83224f4a13d14f05327bbeba78
julianstein <julian.stein@gmail.com>
2021-01-30 23:05:01 -0800

    test dangerouslyinnerhtml

--- components/TheVideo.js
+++ components/TheVideo.js
@@ -4,20 +4,26 @@ const TheVideo = (props, { scrollPosition }) => {
   const { src } = props;
 
   return (
-    <video
-      loop
-      onContextMenu={(e) => {
-        e.preventDefault();
-      }}
-      width="640"
-      height="360"
-      playsInline
-      autoPlay
-      poster={'/images/sketches-stills/' + src + '.jpg'}
-      muted
-      className="absolute w-full h-full align-bottom top-0 object-cover">
-      <source muted="true" src={'/videos/' + src + '.m4v'}></source>
-    </video>
+    <LazyLoadComponent scrollPosition={scrollPosition}>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: `
+        <video
+          loop
+          muted
+          autoplay
+          playsInline
+          width="640"
+          height="360"
+          poster="${'/images/sketches-stills/' + src + '.jpg'}"
+          class="absolute w-full h-full align-bottom top-0 object-cover"
+        
+        >
+        <source src="${'/videos/' + src + '.m4v'}" type="video/mp4" />
+        </video>`
+        }}
+      />
+    </LazyLoadComponent>
   );
 };
 
