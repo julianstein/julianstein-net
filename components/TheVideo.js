3246eae8428ad968c85a22b377e6202912cf11a4
julianstein <julian.stein@gmail.com>
2021-01-30 23:10:52 -0800

    it was the low battery

--- components/TheVideo.js
+++ components/TheVideo.js
@@ -5,24 +5,20 @@ const TheVideo = (props, { scrollPosition }) => {
 
   return (
     <LazyLoadComponent scrollPosition={scrollPosition}>
-      <div
-        dangerouslySetInnerHTML={{
-          __html: `
-        <video
-          loop
-          muted
-          autoplay
-          playsInline
-          width="640"
-          height="360"
-          poster="${'/images/sketches-stills/' + src + '.jpg'}"
-          class="absolute w-full h-full align-bottom top-0 object-cover"
-        
-        >
-        <source src="${'/videos/' + src + '.m4v'}" type="video/mp4" />
-        </video>`
+      <video
+        loop
+        playsInline
+        autoPlay
+        muted
+        onContextMenu={(e) => {
+          e.preventDefault();
         }}
-      />
+        width="640"
+        height="360"
+        poster={'/images/sketches-stills/' + src + '.jpg'}
+        className="absolute w-full h-full align-bottom top-0 object-cover">
+        <source src={'/videos/' + src + '.m4v'}></source>
+      </video>
     </LazyLoadComponent>
   );
 };
