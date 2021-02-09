456163e7257c407886c386ef8188aa8e5915c5e3
julianstein <julian.stein@gmail.com>
2021-02-08 19:06:26 -0800

    try mobile hack for scroll

--- pages/_app.js
+++ pages/_app.js
@@ -2,37 +2,43 @@ import '../styles/index.scss';
 
 import { AnimatePresence, motion } from 'framer-motion';
 import { Provider } from 'react-redux';
-
+import Head from 'next/head';
 import Layout from '../components/layout';
 import store from '../store';
 
 const App = ({ Component, pageProps, router }) => {
   return (
-    <Provider store={store}>
-      <Layout>
-        <AnimatePresence exitBeforeEnter>
-          <motion.div
-            key={router.asPath}
-            initial="pageInitial"
-            animate="pageAnimate"
-            exit="pageExit"
-            transition={{ duration: 0.25 }}
-            variants={{
-              pageInitial: {
-                opacity: 0
-              },
-              pageAnimate: {
-                opacity: 1
-              },
-              pageExit: {
-                opacity: 0
-              }
-            }}>
-            <Component {...pageProps} />
-          </motion.div>
-        </AnimatePresence>
-      </Layout>
-    </Provider>
+    <>
+      <Head>
+        <script src="https://unpkg.com/smoothscroll-polyfill/dist/smoothscroll.min.js"></script>
+        <script src="https://unpkg.com/smoothscroll-anchor-polyfill"></script>
+      </Head>
+      <Provider store={store}>
+        <Layout>
+          <AnimatePresence exitBeforeEnter>
+            <motion.div
+              key={router.asPath}
+              initial="pageInitial"
+              animate="pageAnimate"
+              exit="pageExit"
+              transition={{ duration: 0.25 }}
+              variants={{
+                pageInitial: {
+                  opacity: 0
+                },
+                pageAnimate: {
+                  opacity: 1
+                },
+                pageExit: {
+                  opacity: 0
+                }
+              }}>
+              <Component {...pageProps} />
+            </motion.div>
+          </AnimatePresence>
+        </Layout>
+      </Provider>
+    </>
   );
 };
 
